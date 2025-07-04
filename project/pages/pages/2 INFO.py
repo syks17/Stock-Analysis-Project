200b045c37c8076db6c5d93abd7c1656149fe402
syks17 <134402172+syks17@users.noreply.github.com>
2025-07-04 18:19:39 +0530

    Update 2 INFO.py

--- project/pages/pages/2 INFO.py
+++ project/pages/pages/2 INFO.py
@@ -11,20 +11,17 @@ from yahooquery import Ticker
 
 user_stock = st.session_state.get('user_input_stock')
 period_days = st.selectbox("Select period", ['5y','1y', '2y','10y','max'])
-today = datetime.today().strftime('%Y-%m-%d')
 ticker = Ticker(st.session_state.get('user_input_stock'))
 st_data = ticker.history(period=period_days, interval="1d")
 df = pd.DataFrame(st_data)
-
-
 st.write("")
 
 
-price_data , Info , = st.tabs(["Price data","Info"])
+price_data , Info , metrics = st.tabs(["Price data","Info","Metrics"])
+
 with price_data:
-    col1,col2,col3 = st.columns(3)
     daily_change = df['close'].iloc[-1] - df['close'].iloc[-2]
-    col1.metric('Daily Change',str(round(df['close'].iloc[-1],2)), str(round(daily_change,2)))
+    st.metric('Daily Change',str(round(df['close'].iloc[-1],2)), str(round(daily_change,2)))
 
 
     st.subheader('Closing Price Chart')
@@ -55,27 +52,43 @@ with price_data:
     st.pyplot(fig2)
 
 
+key_stats = ticker.key_stats.get(user_stock, {})    
+financial_data = ticker.financial_data.get(user_stock, {})
+profile = ticker.asset_profile.get(user_stock, {})
+
 with Info:
     st.subheader(user_stock)
     if user_stock:
         try:
-            stock = Ticker(user_stock)
-
-            profile = stock.asset_profile.get(user_stock, {})
             long_summary = profile.get("longBusinessSummary", "No summary available.")
             sector = profile.get("sector", "N/A")
             website = profile.get("website", "N/A")
+            mlt_cap = profile.get("marketCap","N/A")
 
 
             st.write(long_summary)
             st.markdown(f"**Sector**: {sector}")
             st.markdown(f"**Website**: {website}")
+            st.markdown(f"**Market Cap:** {mlt_cap}")
      
 
         except Exception as e:
             st.error(f"Error: {e}")
 
-    
-    
 
+with metrics:
+    col1,col2 = st.columns(2)
 
+    with col1:
+        st.metric("ROA", f"{financial_data.get('returnOnAssets', 0):.2%}")
+        st.metric("Operating Margin", f"{financial_data.get('operatingMargins', 0):.2%}")
+        st.metric("Revenue", f"${financial_data.get('totalRevenue', 0):,}")
+        st.metric("ROE", f"{financial_data.get('returnOnEquity', 0):.2%}")
+        st.metric("Profit Margin", f"{financial_data.get('profitMargins', 0):.2%}")
+
+    with col2:
+        st.metric("Gross Margin", f"{financial_data.get('grossMargins', 0):.2%}")
+        st.metric("Current Ratio", f"{financial_data.get('currentRatio', 0):.2f}")
+        st.metric("Debt to Equity", f"{financial_data.get('debtToEquity', 0):.2f}")
+        st.metric("P/E Ratio (TTM)", f"{key_stats.get('trailingPE', 'N/A')}")
+        st.metric("EPS (TTM)", f"{key_stats.get('trailingEps', 'N/A')}")
