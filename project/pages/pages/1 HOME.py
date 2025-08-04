82bc39330b150e9a4513581a446a570c40383f16
Shrey <134402172+syks17@users.noreply.github.com>
2025-08-04 22:32:24 +0530

    Update 1 HOME.py
    added error handling

--- project/pages/pages/1 HOME.py
+++ project/pages/pages/1 HOME.py
@@ -5,24 +5,44 @@ from datetime import datetime
 import numpy as np
 from yahooquery import Ticker
 
-
+#STOCK
+st.warning("If Network/SSL error occurs Please refresh the page")
+st.write("")
 st.subheader("Stock Ticker")
 if 'user_input_stock' not in st.session_state:
     st.session_state.user_input_stock = ''
 st.session_state.user_input_stock = st.text_input('Enter Stock Ticker',"GOOG", key="key").upper()
 st.write("*Current Ticker* :",st.session_state.user_input_stock)
 ticker = Ticker(st.session_state.get('user_input_stock'))
-st_data = ticker.history(period="1y", interval="1d")
-df = pd.DataFrame(st_data)
-current_price = df['close'].iloc[-1]
+if ticker:
+    try:
+        st_data = ticker.history(period="1y", interval="1d")
+        df = pd.DataFrame(st_data)
+        current_price = df['close'].iloc[-1]
+        st.write("Current Price",current_price.round(2))
+    except Exception:
+        st.error("No data found for the ticker. Please check the ticker symbol.")
+
 
 st.write("")
 
+#INDEX
 st.subheader("Index Ticker",)
 if 'user_input_mkt' not in st.session_state:
     st.session_state.user_input_mkt = ''
 today = datetime.today().strftime('%Y-%m-%d')
 st.session_state.user_input_mkt = st.text_input('Enter Market Ticker', "^GSPC", key="key_mkt").upper()
+ticker_mkt = Ticker(st.session_state.get('user_input_mkt'))
 st.write("*Current Index Ticker* : ",st.session_state.user_input_mkt)
-st.warning("Please make sure to add `^` before the index ticker")
+if ticker_mkt:
+    try:
+        st_data = ticker_mkt.history(period="1y", interval="1d")
+        dfmkt = pd.DataFrame(st_data)
+        current_price_mkt = dfmkt['close'].iloc[-1]
+        st.write("Current Price",current_price_mkt.round(2))
+    except Exception:
+        st.error("No data found for the index ticker. Please check the ticker symbol. Make sure to add `^` before the index ticker")
+
+st.write("")
+
 
